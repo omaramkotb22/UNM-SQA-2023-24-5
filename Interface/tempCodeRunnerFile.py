15f39925afb329bdb47db62cb605f432f1bf4eb9
YepJohnWayne <efyjy8@nottingham.edu.my>
2023-10-16 13:38:06 +0800

    Layout

--- Interface/tempCodeRunnerFile.py
+++ Interface/tempCodeRunnerFile.py
@@ -8,10 +8,10 @@ def on_configure(event):
 
 def on_mousewheel(event):
     current_position = canvas.yview()
-    if event.delta > 0 and current_position != (0.0, 0.0):
+    if event.delta > 0 and current_position[0] > 0:
         # Scroll up only if not already at the top
         canvas.yview_scroll(-1 * (event.delta // 120), "units")
-    elif event.delta < 0 and current_position != (1.0, 1.0):
+    elif event.delta < 0 and current_position[1] < 1:
         # Scroll down only if not already at the bottom
         canvas.yview_scroll(-1 * (event.delta // 120), "units")
 
@@ -22,9 +22,6 @@ def button_action():
 root = tk.Tk()
 root.title("Video Player")
 root.geometry("1920x1050")  # Adjust the size for better visibility
-
-# Set the window attributes to allow resizing
-root.state('zoomed')
 
 # Create a Frame for the left side (1/4 of the window)
 left_frame = ttk.Frame(root)
@@ -72,7 +69,7 @@ right_frame = ttk.Frame(root)
 right_frame.grid(row=0, column=1, sticky="nsew")
 
 # Add content to the right side
-right_label = ttk.Label(right_frame, text="Currently Playing:")
+right_label = ttk.Label(right_frame, text="Non-Scrollable Right Side")
 right_label.pack()
 
 # Configure grid weights to allow resizing
