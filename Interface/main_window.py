c137ab50e1528424d00e8b5f51431e39e352dee1
yeeenzii <ynhlabatsi@gmail.com>
2023-11-05 23:38:04 +0800

    add and save notes, view when opening video

--- Interface/main_window.py
+++ Interface/main_window.py
@@ -1,258 +1,244 @@
 import sys
-
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 import os
-import sys
-
 import requests
+import sqlite3
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
-
 parent_dir = os.path.dirname(current_dir)
-
-sys.path.append(os.path.join(parent_dir, 'youtube_api')) #adds youtube_api to path
+sys.path.append(os.path.join(parent_dir, 'youtube_api'))  # Adds youtube_api to path
+db_path = os.path.join(parent_dir, 'Notes.db')  
 import youtube
-
 
 youtube = youtube.Youtube()
 
-
-# Class for video
-
-# class Video:
-
-#     def __init__(self, image_path, title, video_id):
-
-#         self.image_path = image_path
-
-#         self.title = title
-
-#         self.video_id = video_id
-
-
-
+# adds video widget (thumbnail + title) in collection
 class VideoEntry(QWidget):
-
     clicked = pyqtSignal(str)
 
     def __init__(self, video):
-
-        super().__init__() # Inherits from the constructor of the QWidget class
-
-        self.video = video # Video object
-
-        self.initUI() # Call the initUI method to initialize the user interface
-
+        super().__init__()
+        self.video = video
+        self.initUI()
 
     def initUI(self):
-
         layout = QHBoxLayout()
-
-        # label for the image
-
-        image_label = QLabel(self) # creates label
-
-        response = requests.get(self.video.image_path) # gets image from url
-
-        image_data = response.content # gets image data
-
+        image_label = QLabel(self)
+        response = requests.get(self.video.image_path)
+        image_data = response.content
         pixmap = QPixmap()
-
-        pixmap.loadFromData(image_data) #sets image to pixmap
-
-        image_label.setPixmap(pixmap) #sets image to label
-
-        # label for the title
+        pixmap.loadFromData(image_data)
+        image_label.setPixmap(pixmap)
 
         title_label = QLabel(self.video.title)
-
         title_label.setFont(QFont("Roboto", 18))
-
         image_label.mousePressEvent = lambda event: (self.clicked.emit(self.video.video_id))
 
-
-        # Add both the image and title labels to the horizontal layout
-
         layout.addWidget(image_label)
-
         layout.addWidget(title_label)
-
-
         self.setLayout(layout)
-class MainWindow(QMainWindow):
 
-    #list of videos and their info (filled in by youtube api)
-
-    global videos_list
-
-    videos_list = youtube.search()  #searches for videos with "Software Quality Assurance" in the title
-
-    for video in videos_list:
-
-        print(video.title)
-
-        print(video.video_id)
-
-        print(video.image_path)
-        print()
+class SplitWindow(QMainWindow):
+    selected_keywords = set()  # Set to store selected keywords
 
     def __init__(self):
-
-        super().__init__() # Inherits from the constructor of the QMainWindow class
-
-
+        super().__init__()
         self.setWindowTitle("Video Player")
-
         self.setGeometry(100, 100, 1920, 1080)
 
-
         central_widget = QWidget(self)
-
         self.setCentralWidget(central_widget)
 
-
         central_layout = QVBoxLayout()
-
         central_widget.setLayout(central_layout)
-
 
         splitter = QSplitter(Qt.Horizontal)
 
-
         left_widget = QWidget(splitter)
 
-        # right_widget = QWidget(splitter)
-
-
         left_widget.setStyleSheet("background-color: lightblue")
-
         left_widget.setMinimumWidth(300)
 
-
         left_layout = QVBoxLayout(left_widget)
-
         left_widget.setLayout(left_layout)
 
-
         title_label = QLabel("Video Collection", left_widget)
-
         title_label.setFont(QFont("Roboto", 20))
-
         title_label.setMinimumWidth(650)
-
         title_label.setAlignment(Qt.AlignCenter)
 
+        filter_frame = QFrame()
+        filter_frame.setFrameShape(QFrame.StyledPanel)
+        filter_layout = QVBoxLayout(filter_frame)
 
-        line = QFrame()
+        keyword_label = QLabel("Keywords")
+        keyword_label.setFont(QFont("Roboto", 14))
+        filter_layout.addWidget(keyword_label)
 
-        line.setFrameShape(QFrame.HLine)
+        self.predefined_keywords = ["explained", "tutorial", "demo"]
+        self.predefined_checkboxes = []
 
-        line.setFrameShadow(QFrame.Sunken)
-
-
-        scroll_area = QScrollArea()
-
-        scroll_area.setWidgetResizable(True)
-
-        scroll_area.setFrameShape(QFrame.NoFrame)  # Set the frame shape to NoFrame
-
-        scroll_area.setFrameShadow(QFrame.Plain) 
-
-        content_widget = QWidget()
-
-        content_layout = QVBoxLayout(content_widget)
-
-        scroll_area.setWidget(content_widget)
-
-
-        # Add videos from youtube search to layout
-
-        for video in videos_list:
-
-            video_entry = VideoEntry(video)
-
-            video_entry.clicked.connect(self.playVideo)
-
-            content_layout.addWidget(video_entry)
-            
+        for keyword in self.predefined_keywords:
+            checkbox = QCheckBox(keyword)
+            checkbox.setFont(QFont("Roboto", 12))
+            checkbox.stateChanged.connect(self.updateSelectedKeywords)
+            self.predefined_checkboxes.append(checkbox)
+            filter_layout.addWidget(checkbox)
 
 
         left_layout.addWidget(title_label)
+        left_layout.addWidget(filter_frame)
+
+        line = QFrame()
+        line.setFrameShape(QFrame.HLine)
+        line.setFrameShadow(QFrame.Sunken)
+
+        scroll_area = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setFrameShape(QFrame.NoFrame)
+        scroll_area.setFrameShadow(QFrame.Plain)
+        content_widget = QWidget()
+        self.content_layout = QVBoxLayout(content_widget)
+        scroll_area.setWidget(content_widget)
 
         left_layout.addWidget(line)
-
         left_layout.addWidget(scroll_area)
 
-
-        # right_widget.setStyleSheet("background-color: lightgray")
-
-
-        # right_label = QLabel("Currently Playing:", right_widget)
-
-        # right_label.setFont(QFont("Roboto", 20))
-
-        # right_layout = QVBoxLayout(right_widget)
-
-        # right_layout.addWidget(right_label, alignment=Qt.AlignTop | Qt.AlignHCenter)
-
-        # playArea = QWebEngineView()
-
-        # right_layout.addWidget(playArea)    #dedicated play area (initially blank)
-
-        # right_widget.setLayout(right_layout)
-        
-
-
         splitter.addWidget(left_widget)
-
-        # splitter.addWidget(right_widget)
-
-
-        splitter.setSizes([300, 500])
-
-
         central_layout.addWidget(splitter)
-     
 
-     #embed selected video onto dedicated play area   
+        self.updateVideoList(youtube.search())
 
+    # popup video player (appears when video in collection clicked)
     def playVideo(self, id):
+        embed_code = f'<iframe width="900" height="600" src="https://www.youtube.com/embed/{id}?hl=en" frameborder="0" allowfullscreen></iframe>'
+        popup = QDialog(self)
+        popup.setWindowTitle("YouTube Video")
+        popup.setMinimumSize(1450, 650)
+        layout = QHBoxLayout(popup)
+        splitter2 = QSplitter(Qt.Horizontal)
+        
+        noteFrame = QFrame()
+        noteFrame.setWindowTitle("Notepad")
+        self.noteLayout = QVBoxLayout(noteFrame)
+        label = QLabel("Notepad",noteFrame)
+        label.setFont(QFont("Roboto", 18))
+        self.noteLayout.addWidget(label)
+        
+        self.noteView = QScrollArea(noteFrame)
+        self.noteContainer = QWidget(self.noteView)
+        self.containerLayout = QVBoxLayout(self.noteContainer)
+        notes = self.checkDB(id)
+        self.notesDisplay(notes, False)
+        self.noteLayout.addWidget(self.noteView)
+        
+        saveButton = QPushButton(noteFrame, text="Save")
+        self.noteLayout.addWidget(saveButton)
+        saveButton.clicked.connect(lambda: self.saveNote(id, saveButton))
+        addButton = QPushButton(noteFrame, text="Add Note")
+        addButton.clicked.connect(lambda: self.notesDisplay(notes, True))
+        self.noteLayout.addWidget(addButton)
+        
+        
+        
+        
+        playArea = QWebEngineView()
+        playArea.setHtml(embed_code)
+        
+        splitter2.addWidget(playArea)
+        splitter2.addWidget(noteFrame)
+        splitter2.setSizes([900, 500])
+        
+        layout.addWidget(splitter2)
+        popup.setLayout(layout)
+        popup.exec_()
 
-            embed_code = f'<iframe width="900" height="600" src="https://www.youtube.com/embed/{id}?hl=en" frameborder="0" allowfullscreen></iframe>'
+    # filter video collection based on keywords
+    def updateSelectedKeywords(self):
+        self.selected_keywords.clear()
+        for checkbox in self.predefined_checkboxes:
+            if checkbox.isChecked():
+                self.selected_keywords.add(checkbox.text())
+        self.applyKeywordFilter()
 
+    def applyKeywordFilter(self):
+        selected_keywords = list(self.selected_keywords)  # Convert set to list
 
-            # Create a popup with a dedicated play area
+        if not selected_keywords:  # If no keywords are selected, show all 12 videos
+            self.updateVideoList(youtube.search())
+        else:
+            filtered_videos = [video for video in youtube.search() if any(keyword in video.title.lower() for keyword in selected_keywords)]
+            self.updateVideoList(filtered_videos)
 
-            popup = QDialog(self)
+    def updateVideoList(self, filtered_videos):
+        for i in reversed(range(self.content_layout.count())):
+            widget = self.content_layout.itemAt(i).widget()
+            if widget:
+                widget.deleteLater()
 
-            popup.setWindowTitle("YouTube Video")
+        for video in filtered_videos:
+            video_entry = VideoEntry(video)
+            video_entry.clicked.connect(self.playVideo)
+            self.content_layout.addWidget(video_entry)
+    
+    def checkDB(self, id):
+        db = sqlite3.connect(db_path)
+        cursor = db.cursor()
+        cursor.execute('select Note from Video_Notes where id = ?', (id,))
+        result = cursor.fetchall()
+        if result:
+            print(result)
+            return result
+         
+    def saveNote(self, id, saveButton):
+        note = self.textArea.toPlainText()
+        try:
+            db = sqlite3.connect(db_path)
+            cursor = db.cursor()
+            data_tuple = (id, note)
+            cursor.execute('insert into Video_Notes values (?,?)', data_tuple)
+            db.commit()
+            saveButton.setText("Saved")
+            cursor.close()
 
-            popup.setMinimumSize(950, 650)
+        finally:
+            if db:
+                db.close()
 
-            layout = QVBoxLayout(popup)
-
-            playArea = QWebEngineView()
-
-            playArea.setHtml(embed_code)
+    def notesDisplay(self, notes, add):
+        if add:
+            if notes:
+                i = 0
+                while i < len(notes):
+                    widget = self.containerLayout.itemAt(i).widget()
+                    if widget:
+                        widget.deleteLater()
+                    i += 1
+                    
+                for x in notes:
+                    self.textArea = QTextEdit(self.noteContainer)
+                    self.textArea.lineWrapColumnOrWidth = 50
+                    self.textArea.setText(x[0])
+                    self.containerLayout.addWidget(self.textArea)
+            self.textArea = QTextEdit(self.noteContainer)
+            self.textArea.lineWrapColumnOrWidth = 50
+            self.containerLayout.addWidget(self.textArea)
+        else:
+            if notes:
+                for x in notes:
+                    self.textArea = QTextEdit(self.noteContainer)
+                    self.textArea.lineWrapColumnOrWidth = 50
+                    self.textArea.setText(x[0])
+                    self.containerLayout.addWidget(self.textArea)
+        
+        add = False
             
-            copy_button = QPushButton("Copy URL", self)
-            copy_button.setFont(QFont("Roboto", 12))
-            copy_button.setStyleSheet("background-color: lightblue")
-            copy_button.resize(100,32)
-            copy_button.move(900, 600)
-            copy_button.clicked.connect(self.copyURL(id))
 
-            layout.addWidget(playArea)
-
-            popup.setLayout(layout)
-
-            popup.exec_()
-            
-
-def copyURL(self, id):
-    url = f'https://www.youtube.com/watch?v={video.id}'
-    pyperclip.copy(url)
-    print("URL copied to clipboard")
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = SplitWindow()
+    window.showMaximized()
+    sys.exit(app.exec_())
